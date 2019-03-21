b66cf6c2f0c9493851b9e03ffd5f97fdcbe89b55
Gregory Dukes <kcils360@gmail.com>
2019-03-20 17:21:02 -0700

    part 1 complete

--- edit-file.js
+++ edit-file.js
@@ -2,16 +2,31 @@
 
 let fs = require('fs');
 let file = `${__dirname}/files/test.txt`;
+let newWords = 'the brown dog ate the stupid fox'
 let cliFile = process.argv[2];
+
+//code for bringing in the file name from the command line found at https://nodejs.org/docs/latest/api/process.html#process_process_argv
+  process.argv.forEach((val, index) => {
+  });
 
   let readTheStupidFile = (err,data) => {
     if (err) {
       throw err;
     }
     console.log(data.toString());
-  };
-  
-  process.argv.forEach((val, index) => {
-  });
+    fs.writeFile(cliFile, newWords, (err) => {
+        if (err) {
+            throw err;
+        }
+        fs.readFile(cliFile, (err,data) => {
+            if (err) {
+                throw err;
+              }
+            console.log(data.toString());
+        })
+    })
+};
 
+
+  
 fs.readFile(cliFile, readTheStupidFile);
